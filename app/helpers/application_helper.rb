fea3419ff2fa2db198dfefe031b1e5572f40e649
shimura <shimura21ngs@gmail.com>
2022-08-23 23:06:33 +0900

    complete 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
-app/helpers/application_helper.rb
 module ApplicationHelper
 
-  # ページごとの完全なタイトルを返します。
+  # ページごとの完全なタイトルを返します。               
   def full_title(page_title = '')                     # メソッド定義とオプション引数
     base_title = "Ruby on Rails Tutorial Sample App"  # 変数への代入
     if page_title.empty?                              # 論理値テスト
@@ -10,5 +9,5 @@ module ApplicationHelper
       page_title + " | " + base_title                 # 文字列の結合
     end
   end
-
+  
 end
