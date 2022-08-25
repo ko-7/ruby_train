ac1aca4c9b0dd13c23849efebe3f6a239f441659
shimura <shimura21ngs@gmail.com>
2022-08-25 21:43:40 +0900

    complete 6

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  # validates(:name, presence: true)
+  validates :name, 
+    presence: true,           #入力必須
+    length: { maximum: 50 }   #50文字以内にするバリデーション
+
+
+  before_save { self.email = self.email.downcase }
+  # ↑の書き換え：before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, 
+    presence: true, 
+    length: { maximum: 255 }, 
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
+
+  validates :password, 
+    presence: true,
+    length: { minimum: 6 }
+
+end
